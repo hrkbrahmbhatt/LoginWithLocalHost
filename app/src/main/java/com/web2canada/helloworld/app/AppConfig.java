da151609fedadfaa2ee149a1e5ae39b2e4566623
Hrsikesh Brahmbhatt <hrkbrahmbhatt@gmail.com>
2018-03-25 20:57:48 -0400

    Initial commit

---
+++ app/src/main/java/com/web2canada/helloworld/app/AppConfig.java
@@ -0,0 +1,15 @@
+package com.web2canada.helloworld.app;
+
+/**
+ * Created by Security on 25/03/2018.
+ */
+
+public class AppConfig {
+
+    // Server user login url
+    public static String URL_LOGIN = "http://10.0.2.2:80/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://10.0.2.2:80/android_login_api/register.php";
+
+}
